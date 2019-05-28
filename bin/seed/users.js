9854152788c16ba032c9934fea6a1163b68335e8
Allyn Alda <Teddy@Om.local>
2019-05-28 14:13:13 +0200

    clean the seed file users.js

--- bin/seed/users.js
+++ bin/seed/users.js
@@ -1,8 +1,17 @@
-[
+const mongoose = require('mongoose');
+const User = require('./../../models/user')
+
+require("dotenv").config();
+
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+ keepAlive: true,
+ useNewUrlParser: true,
+ reconnectTries: Number.MAX_VALUE
+});
+
+const users = [
     {
-        "_id": {
-            "$oid": "5ced048f1c2124001796c94a"
-        },
+        "_id": "5ced048f1c2124001796c94a",
         "username": "Allyn",
         "password": "$2b$10$RVNZ3oWESWAkQA.vZlLCGOQfazD5IRZz.nlhGrrRcvJKYS7tmRUpS",
         "firstName": "Allyn",
@@ -10,18 +19,9 @@
         "email": "allyn.alda@gmail.com",
         "contact": 60674120,
         "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1559037060/demo/soqibzfgnattil67tze8.jpg",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce55e40334ff7491180f7e7"
-        },
+        "_id": "5ce55e40334ff7491180f7e7",
         "username": "Mario",
         "password": "$2b$10$/B/T8d3lW6.p3Y6NG3tuZep32ydYUKWNqRu94GVyjbw.Lqxsdlr7G",
         "firstName": "Mario",
@@ -29,18 +29,9 @@
         "email": "mario!@live.com",
         "contact": 60674120,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce654730afcba65e24f0e6c"
-        },
+        "_id": "5ce654730afcba65e24f0e6c",
         "username": "Jose",
         "password": "$2b$10$DWxQ6K/s2WWiJsGtiTgRreHcGUxX5EjqMHNhalQleV1O0FYLm.nHe",
         "firstName": "Jose",
@@ -48,18 +39,9 @@
         "email": "jose@hotmail.com",
         "contact": 729973098,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce6553f0afcba65e24f0e6e"
-        },
+        "_id": "5ce6553f0afcba65e24f0e6e",
         "username": "Dorota",
         "password": "$2b$10$oJFa9d00uaHF4A0ELecyOu4od5SaJtCPpM52F1cVq4oUVVLLYZQtG",
         "firstName": "Dorota",
@@ -67,18 +49,9 @@
         "email": "dorota@hotmail.com",
         "contact": 687921123,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce67b6a998bc6744f6ffe17"
-        },
+        "_id": "5ce67b6a998bc6744f6ffe17",
         "username": "Laura",
         "password": "$2b$10$AKtfiHBA.7K/WvqpxvpXTeWniYNH0FKzLrKBIcxTB5rpSe2V7W9MO",
         "firstName": "Laura",
@@ -86,18 +59,9 @@
         "email": "laura@hotmail.com",
         "contact": 678123654,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce67bc2998bc6744f6ffe19"
-        },
+        "_id": "5ce67bc2998bc6744f6ffe19",
         "username": "Elise",
         "password": "$2b$10$AjlYj5swFtTKCKWpHvoNHOhH4XRpWZCQp72dHvcg5/C5HfL3L2lR.",
         "firstName": "Elise",
@@ -105,18 +69,10 @@
         "email": "elise@gmail.com",
         "contact": 837123546,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
         "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce67c52998bc6744f6ffe1b"
-        },
+        "_id": "5ce67c52998bc6744f6ffe1b",
         "username": "Allyn",
         "password": "$2b$10$HV/dC9VGt1OLVRVGB8nedeHyzRAxgYaRaIRTOU1KrxHodIN2FEcCa",
         "firstName": "Alana",
@@ -124,18 +80,9 @@
         "email": "alana@hotmail.com",
         "contact": 650923123,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce799cd39d8c1241eacece4"
-        },
+        "_id": "5ce799cd39d8c1241eacece4",
         "username": "Maria",
         "password": "$2b$10$SgdV4kHLhatjmNkJZ96VleDkfP/W2pd375Q1p1tWvtJTfbw50PfoO",
         "firstName": "Maria",
@@ -143,18 +90,9 @@
         "email": "maria@live.com",
         "contact": 60674120,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ce7b21517a85d0e0c050952"
-        },
+        "_id": "5ce7b21517a85d0e0c050952",
         "username": "Joan",
         "password": "$2b$10$bJzD28OakG9ujMMHRmsbEuDEuEGyc.z3j8pSPHeQZAPlLuSSLrFYe",
         "firstName": "Joan",
@@ -162,18 +100,9 @@
         "email": "joan@live.com",
         "contact": 923123456,
         "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558688276/demo/gqi1kbiexliffopjhvjm.jpg",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ceb91c5df7be9b2de24a1e4"
-        },
+        "_id": "5ceb91c5df7be9b2de24a1e4",
         "username": "Sean",
         "password": "$2b$10$LO1VBqaAi0Q2tMCaC2CL7ul0/SM4DQm/aaX6sbZwoGemb9YG1V7Di",
         "firstName": "Sean",
@@ -181,18 +110,9 @@
         "email": "sean@gmail.com",
         "contact": 830038901,
         "image": "",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
     {
-        "_id": {
-            "$oid": "5ceb9223df7be9b2de24a1e5"
-        },
+        "_id": "5ceb9223df7be9b2de24a1e5",
         "username": "Bryan",
         "password": "$2b$10$V8MlPrS9Kne/rhSQbUBAD.cSuCZh2YlxwrbRnaRjAWB5iOJQkxu/2",
         "firstName": "Bo",
@@ -200,12 +120,14 @@
         "email": "bo@hotmail.com",
         "contact": 902392923,
         "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558942224/demo/ejoawsjlp7datz5h4wyt.jpg",
-        "created_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "updated_at": {
-            "$date": "2019-05-28T09:51:11.074Z"
-        },
-        "__v": 0
     },
 ]
+
+User.create(users)
+.then((user) => {
+    console.log(user);
+  mongoose.connection.close();
+})
+.catch(error => {
+  console.error(error);
+});
