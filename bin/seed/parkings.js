934d7de561fcd5c713eaf9aad47f9154e51e307e
Allyn Alda <Teddy@Om.local>
2019-05-28 13:28:13 +0200

    clean the seed file parkings.js

---
+++ bin/seed/parkings.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const Parking = require('./../../models/Parking')
+
+require("dotenv").config();
+
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+ keepAlive: true,
+ useNewUrlParser: true,
+ reconnectTries: Number.MAX_VALUE
+});
+
+const parking = [{
+    "currentLoc": {
+      "coordinates": [
+        41.3851,
+        2.1734
+      ]
+    },
+    "spaceFor": "car",
+    "usersInterested": [ ],
+    "renterName": "Allyn",
+    "__v": 0,
+    "district": "Barcelona",
+    "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558869399/demo/jkl87vqgrsyzsrgd3kk4.jpg",
+    "location": "Carrer Sant Antoni 43"
+  },
+  {
+    "currentLoc": {
+      "coordinates": [
+        41.4036,
+        2.1744
+      ]
+    },
+    "spaceFor": "car",
+    "usersInterested": [ ],
+    "renterName": "Elise",
+    "__v": 0,
+    "district": "Barcelona",
+    "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558821017/demo/dacxoodt6spr6guhytqv.jpg",
+    "location": "Carrer Mallorca 21"
+  },
+  {
+    "currentLoc": {
+      "coordinates": [
+        41.4145,
+        2.1527
+      ]
+    },
+    "spaceFor": "motorcycle",
+    "usersInterested": [ ],
+    "renterName": "Maria",
+    "__v": 0,
+    "district": "Barcelona",
+    "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558821249/demo/ldpbeifaowkuuvp3uspj.jpg",
+    "location": "Carrer America 21"
+  },
+  {
+    "currentLoc": {
+      "coordinates": [
+        41.3911,
+        2.1806
+      ]
+    },
+    "spaceFor": "car",
+    "usersInterested": [ ],
+    "renterName": "Mario",
+    "__v": 0,
+    "district": "Barcelona",
+    "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558869914/demo/aoy4vtwwqd3a9vwmnfsg.jpg",
+    "location": "Carrer Corsega 12"
+  }]
+
+
+Parking.create(parking)
+.then((spot) => {
+    console.log(spot);
+  mongoose.connection.close();
+})
+.catch(error => {
+  console.error(error);
+});
---
+++ bin/seed/users.js
@@ -0,0 +1,211 @@
+[
+    {
+        "_id": {
+            "$oid": "5ced048f1c2124001796c94a"
+        },
+        "username": "Allyn",
+        "password": "$2b$10$RVNZ3oWESWAkQA.vZlLCGOQfazD5IRZz.nlhGrrRcvJKYS7tmRUpS",
+        "firstName": "Allyn",
+        "lastName": "Alda",
+        "email": "allyn.alda@gmail.com",
+        "contact": 60674120,
+        "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1559037060/demo/soqibzfgnattil67tze8.jpg",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce55e40334ff7491180f7e7"
+        },
+        "username": "Mario",
+        "password": "$2b$10$/B/T8d3lW6.p3Y6NG3tuZep32ydYUKWNqRu94GVyjbw.Lqxsdlr7G",
+        "firstName": "Mario",
+        "lastName": "Lona",
+        "email": "mario!@live.com",
+        "contact": 60674120,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce654730afcba65e24f0e6c"
+        },
+        "username": "Jose",
+        "password": "$2b$10$DWxQ6K/s2WWiJsGtiTgRreHcGUxX5EjqMHNhalQleV1O0FYLm.nHe",
+        "firstName": "Jose",
+        "lastName": "Garcia",
+        "email": "jose@hotmail.com",
+        "contact": 729973098,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce6553f0afcba65e24f0e6e"
+        },
+        "username": "Dorota",
+        "password": "$2b$10$oJFa9d00uaHF4A0ELecyOu4od5SaJtCPpM52F1cVq4oUVVLLYZQtG",
+        "firstName": "Dorota",
+        "lastName": "Sadowska",
+        "email": "dorota@hotmail.com",
+        "contact": 687921123,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce67b6a998bc6744f6ffe17"
+        },
+        "username": "Laura",
+        "password": "$2b$10$AKtfiHBA.7K/WvqpxvpXTeWniYNH0FKzLrKBIcxTB5rpSe2V7W9MO",
+        "firstName": "Laura",
+        "lastName": "Capel",
+        "email": "laura@hotmail.com",
+        "contact": 678123654,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce67bc2998bc6744f6ffe19"
+        },
+        "username": "Elise",
+        "password": "$2b$10$AjlYj5swFtTKCKWpHvoNHOhH4XRpWZCQp72dHvcg5/C5HfL3L2lR.",
+        "firstName": "Elise",
+        "lastName": "McFake",
+        "email": "elise@gmail.com",
+        "contact": 837123546,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce67c52998bc6744f6ffe1b"
+        },
+        "username": "Allyn",
+        "password": "$2b$10$HV/dC9VGt1OLVRVGB8nedeHyzRAxgYaRaIRTOU1KrxHodIN2FEcCa",
+        "firstName": "Alana",
+        "lastName": "Alda",
+        "email": "alana@hotmail.com",
+        "contact": 650923123,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce799cd39d8c1241eacece4"
+        },
+        "username": "Maria",
+        "password": "$2b$10$SgdV4kHLhatjmNkJZ96VleDkfP/W2pd375Q1p1tWvtJTfbw50PfoO",
+        "firstName": "Maria",
+        "lastName": "Josep",
+        "email": "maria@live.com",
+        "contact": 60674120,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ce7b21517a85d0e0c050952"
+        },
+        "username": "Joan",
+        "password": "$2b$10$bJzD28OakG9ujMMHRmsbEuDEuEGyc.z3j8pSPHeQZAPlLuSSLrFYe",
+        "firstName": "Joan",
+        "lastName": "Millan",
+        "email": "joan@live.com",
+        "contact": 923123456,
+        "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558688276/demo/gqi1kbiexliffopjhvjm.jpg",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ceb91c5df7be9b2de24a1e4"
+        },
+        "username": "Sean",
+        "password": "$2b$10$LO1VBqaAi0Q2tMCaC2CL7ul0/SM4DQm/aaX6sbZwoGemb9YG1V7Di",
+        "firstName": "Sean",
+        "lastName": "Paul",
+        "email": "sean@gmail.com",
+        "contact": 830038901,
+        "image": "",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": {
+            "$oid": "5ceb9223df7be9b2de24a1e5"
+        },
+        "username": "Bryan",
+        "password": "$2b$10$V8MlPrS9Kne/rhSQbUBAD.cSuCZh2YlxwrbRnaRjAWB5iOJQkxu/2",
+        "firstName": "Bo",
+        "lastName": "Django",
+        "email": "bo@hotmail.com",
+        "contact": 902392923,
+        "image": "https://res.cloudinary.com/dm5wxw4gu/image/upload/v1558942224/demo/ejoawsjlp7datz5h4wyt.jpg",
+        "created_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "updated_at": {
+            "$date": "2019-05-28T09:51:11.074Z"
+        },
+        "__v": 0
+    },
+]
